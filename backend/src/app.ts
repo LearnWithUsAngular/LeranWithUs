3bc0f445d3a7a9c89e2ce906774c9808c404ec83
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-06 15:33:53 +0630

    login api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,13 +6,17 @@ import subcategory_route from './routes/subcategory_route';
 import user_route from './routes/user_route';
 import instructor_route from './routes/instructor_route';
 import course_route from './routes/course_route';
+import auth_route from './routes/auth_route';
 import error from './middlewares/error';
 import cors from 'cors';
 import multer, { FileFilterCallback } from 'multer';
 import { v4 } from 'uuid';
 import { rootDir } from './utils/deleteFile';
 import path from 'path';
+import passport from 'passport';
 const bodyParser = require('body-parser');
+
+require('./config/passport');
 
 dotenv.config();
 
@@ -59,6 +63,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
+
 app.use(multer({ storage: fileStorage, fileFilter }).fields([
     { name: 'instructorProfile', maxCount: 1 }, 
     { name: 'userProfile', maxCount: 1 }, 
@@ -71,11 +77,12 @@ mongoose
     .connect(process.env.DATABASE || "")
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-        app.use("/api/categories", category_route);
-        app.use("/api/subcategories", subcategory_route);
-        app.use("/api/users", user_route);
-        app.use("/api/instructors", instructor_route);
+        app.use("/api/categories", passport.authenticate('jwt', { session: false }), category_route);
+        app.use("/api/subcategories", passport.authenticate('jwt', { session: false }), subcategory_route);
+        app.use("/api/users", passport.authenticate('jwt', { session: false }), user_route);
+        app.use("/api/instructors", passport.authenticate('jwt', { session: false }), instructor_route);
         app.use("/api/courses", course_route);
+        app.use("/api", auth_route);
         app.use(error)
     }
     );
---
+++ backend/src/routes/auth_route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createUser } from '../controllers/UserController';
+import { body } from 'express-validator';
+import { logout, login } from '../controllers/AuthController';
+
+const router = express.Router();
+
+router
+    .route("/login")
+    .post(
+        [
+            body("email").notEmpty().withMessage("Email must not be empty"),
+            body("password").notEmpty().withMessage("Password must not be empty")
+        ],
+        login);
+
+router
+    .route("/logout")
+    .post(logout);
+
+router
+    .route("/signup")
+    .post(
+        [
+            body("name").notEmpty().withMessage("Name must not be empty"),
+            body("email").notEmpty().withMessage("Email must not be empty"),
+            body("password").notEmpty().withMessage("Password must not be empty")
+        ],
+        createUser);
+
+export default router;
