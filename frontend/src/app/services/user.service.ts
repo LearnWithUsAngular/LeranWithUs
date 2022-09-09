459205400da761bc3a4fc0e5d695bffcda29dcba
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-09 15:51:54 +0630

    service file created

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  createAccount(payload: any): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/signup`, payload);
+  }
+}
