48a72f61abbaa0392008b6e7fa9b061b5b47e3bc
Aye Aye Thet <scm.ayeayethet@gmail.com>
2022-09-12 15:14:11 +0630

    #10 Category-list UI

---
+++ frontend/src/app/pages/category-list/category-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { category } from 'src/app/constants/learn';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit {
+
+  categories: any = [];
+  p: number = 1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.categories = category;
+  }
+
+  onMOver(event: MouseEvent) {
+    const card = <HTMLDivElement>event.target;
+    const parent = <HTMLDivElement>card.parentElement;
+
+    parent.style.zIndex = '10';
+  }
+  onMOut(event: MouseEvent) {
+    const card = <HTMLDivElement>event.target;
+    const parent = <HTMLDivElement>card.parentElement;
+
+    parent.style.zIndex = '0';
+  }
+}
