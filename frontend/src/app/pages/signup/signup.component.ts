37a62441959af5463bf99e2454e48dd381ce0d62
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-12 11:25:30 +0630

    bind api with signup

--- frontend/src/app/pages/signup/signup.component.ts
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-signup',
@@ -10,6 +11,7 @@ import { UserService } from 'src/app/services/user.service';
 export class SignupComponent implements OnInit {
 
   signupForm!:FormGroup;
+  signupErr = "";
   constructor(
     private fb : FormBuilder,
     private router: Router,
@@ -25,26 +27,26 @@ export class SignupComponent implements OnInit {
         Validators.pattern('[A-Za-z0-9]{8,}$')]],
         tips:['']
     });
-
   }
 
   get myForm() {
     return this.signupForm.controls;
   }
   createAcc(){
-    // const payload = {
-    //   name: this.signupForm.controls['username'].value,
-    //   email: this.signupForm.controls['email'].value,
-    //   password: this.signupForm.controls['password'].value
-    // }
-    // this.userService.createAccount(payload).then((dist) => {
-    // })
-    this.userService.createAccount({
-      name: this.signupForm.value.username,
-      email: this.signupForm.value.email,
-      password: this.signupForm.value.password
+    const payload = {
+      name: this.signupForm.controls['name'].value,
+      email: this.signupForm.controls['email'].value,
+      password: this.signupForm.controls['password'].value
+    }
+    this.userService.createAccount(payload).subscribe({
+      next: result => {
+        console.log(result)
+        this.router.navigate(['/login']);
+      },
+      error: err => {
+        this.signupErr = "Duplicate Gmail Error. Please type correct Gmail"
+      }
     })
-    this.router.navigate(['/login']);
   }
 
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, retry } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,12 +9,19 @@ import { environment } from 'src/environments/environment';
 export class UserService {
 
   constructor(private http: HttpClient) { }
+  token!: string;
+  ngOnInit(): void {
+     this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiQyYiQxMiRwOXRzUHFVd0RON3dLLzhXdXZpNlZlek8wNW1LbGc2V2NQU1ZGQWpvVEZDRXlzM3hieThlVyIsImlkIjoiJDJiJDEyJEtoSzJkV3ZFSGJia0o5WjVGTTZzNGVhT3QxUmZrUHFBd3RRVE9UNThsSjNuRmhzSlNsVVlLIiwiaWF0IjoxNjYyOTU1NTE5LCJleHAiOjE2NjMwNDE5MTl9.JqEAFzlctKDRDr4wi9mHICT8-Mfm5eldwDgjZUrUB4g";
+  }
+  headerOptions = new HttpHeaders()
+    .set('Authorization', `Bearer ${this.token}`);
+  options = { headers: this.headerOptions };
 
   createAccount(payload: any): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/signup`, payload);
+    return this.http.post(`${environment.apiUrl}/signup`, payload).pipe(retry(3));
   }
 
-  getUsers(payload: any): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/getUser`, payload);
+  getUsers(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/getUser`, this.options);
   }
 }
