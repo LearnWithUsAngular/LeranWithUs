b5ee21572dea7a8066e828dd564b8c53d95d20f2
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-12 13:13:19 +0630

    forget password data bind with api

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -10,6 +10,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
   token!: string;
+
   ngOnInit(): void {
      this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiQyYiQxMiRwOXRzUHFVd0RON3dLLzhXdXZpNlZlek8wNW1LbGc2V2NQU1ZGQWpvVEZDRXlzM3hieThlVyIsImlkIjoiJDJiJDEyJEtoSzJkV3ZFSGJia0o5WjVGTTZzNGVhT3QxUmZrUHFBd3RRVE9UNThsSjNuRmhzSlNsVVlLIiwiaWF0IjoxNjYyOTU1NTE5LCJleHAiOjE2NjMwNDE5MTl9.JqEAFzlctKDRDr4wi9mHICT8-Mfm5eldwDgjZUrUB4g";
   }
