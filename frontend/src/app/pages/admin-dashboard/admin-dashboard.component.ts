078c11d82e61dfaa70b6e07649bf2c8944b9f233
Aye Aye Thet <scm.ayeayethet@gmail.com>
2022-09-15 14:23:14 +0630

    #14 change charts design

--- frontend/src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,38 +8,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-  public primaryXAxis: any;
-  public chartData: any;
-  // public title: string = '';
-  public primaryYAxis: any;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.chartData = [
-      { months: "Jan", category: 30 },
-      { months: "Feb", category: 25 },
-      { months: "Mar", category: 10 },
-      { months: "Apr", category: 50 },
-      { months: "May", category: 50 },
-      { months: "Jun", category: 40 },
-      { months: "Jul", category: 40 },
-      { months: "Aug", category: 30 },
-      { months: "Sep", category: 10 },
-      { months: "Oct", category: 5 },
-      { months: "Nov", category: 3 },
-      { months: "Dec", category: 8 },
-    ];
-    this.primaryXAxis = {
-      valueType: 'Category',
-      title: 'Months'
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];;
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Category',
+        data: [65, 59, 20, 25, 80, 81, 10, 56, 55, 40, 30, 15],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+          'rgba(190, 27, 51,0.2)',
+          'rgba(196, 136, 145,0.2)',
+          'rgba(0, 255, 255,0.2)',
+          'rgba(0, 255, 98,0.2)',
+          'rgba(87, 87, 6,0.2)'
+        ], borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+          'rgb(190, 27, 51)',
+          'rgb(196, 136, 145)',
+          'rgb(0, 255, 255)',
+          'rgb(0, 255, 98)',
+          'rgb(87, 87, 6)'
+        ],
+        borderWidth: 1
+      }]
     };
-    this.primaryYAxis = {
-      minimum: 0,
-      maximum: 80,
-      interval: 10,
-      title: 'Category'
-    };
+
+    const myChart = new Chart("myChart", {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
-
-
 }
