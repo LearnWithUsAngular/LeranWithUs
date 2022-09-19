c2040f864df0244461c49507fcc4d215adcafca8
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-19 13:58:15 +0630

    splid component admin sidenav

--- frontend/src/app/components/subcategory-admin/subcategory-admin.module.ts
+++ frontend/src/app/components/subcategory-admin/subcategory-admin.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SubcategoryAdminRoutingModule } from './subcategory-admin-routing.module';
 import { SubcategoryAdminComponent } from './subcategory-admin.component';
 import { AngularMaterialsModule } from 'src/app/angular-materials.module';
-
-
+import { AdminSidenavModule } from '../admin-sidenav/admin-sidenav.module';
 @NgModule({
   declarations: [SubcategoryAdminComponent],
   imports: [
     CommonModule,
     SubcategoryAdminRoutingModule,
-    AngularMaterialsModule
+    AngularMaterialsModule,
+    AdminSidenavModule
   ]
 })
 export class SubcategoryAdminModule { }
--- frontend/src/app/components/user-admin/user-admin.module.ts
+++ frontend/src/app/components/user-admin/user-admin.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserAdminRoutingModule } from './user-admin-routing.module';
 import { UserAdminComponent } from './user-admin.component';
 import { AngularMaterialsModule } from 'src/app/angular-materials.module';
-
-
+import { AdminSidenavModule } from '../admin-sidenav/admin-sidenav.module';
 @NgModule({
   declarations: [UserAdminComponent],
   imports: [
     CommonModule,
     UserAdminRoutingModule,
-    AngularMaterialsModule
+    AngularMaterialsModule,
+    AdminSidenavModule
   ]
 })
 export class UserAdminModule { }
