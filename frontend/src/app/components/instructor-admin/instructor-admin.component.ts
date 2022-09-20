65f622080b9a532aaf404bc5842021b97c38aa12
Aye Aye Thet <scm.ayeayethet@gmail.com>
2022-09-20 10:17:13 +0630

    #58 data binding in admin dashboard

--- frontend/src/app/components/instructor-admin/instructor-admin.component.ts
+++ frontend/src/app/components/instructor-admin/instructor-admin.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { instructorList } from 'src/app/constants/learn';
+import { InstructorServiceService } from 'src/app/services/instructor-service.service';
+import { DeleteInstructorComponent } from '../delete-instructor/delete-instructor.component';
 
 @Component({
   selector: 'app-instructor-admin',
@@ -10,33 +14,68 @@ import { instructorList } from 'src/app/constants/learn';
 })
 export class InstructorAdminComponent implements OnInit {
 
-  displayedColumns: string[] = ['name', 'age', 'degree', 'gender', 'language', 'action'];
-  dataSource: any;
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['instructorName', 'headline', 'biography', 'createdAt', 'action'];
+  instructorList: any;
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  constructor(
+    public instructorSvc: InstructorServiceService,
+    public router: Router,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.dataSource = new MatTableDataSource(instructorList);
+    this.getInstructor();
+  }
+
+  /**
+  * get instructor data
+  */
+  getInstructor() {
+    this.instructorSvc.getInstructors().subscribe((dist) => {
+      this.instructorList = dist.data;
+      this.dataSource = new MatTableDataSource(this.instructorList);
       this.dataSource.paginator = this.paginator;
-    }, 1000);
+    })
   }
 
-  applyFilter(event: Event) {
-
-  }
-
+  /**
+  * create instructor form
+  */
   createInstructor() {
-
+    this.router.navigate(["/create-instructor"]);
   }
 
+  /**
+  * update instructor form
+  * @param id
+  */
   updateInstructor(id: any) {
-
+    console.log(id)
+    this.router.navigate(['/edit-instructor/' + id]);
   }
 
-  deleteInstructor(id: any) {
-
+  /**
+  * delete instructor 
+  * @param data
+  */
+  deleteInstructor(data: any) {
+    const id = data._id;
+    let dialogRef = this.dialog.open(DeleteInstructorComponent, {
+      width: '30%',
+      data: data,
+    });
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data) {
+        this.instructorSvc.deleteInstructor(id).subscribe((dist) => {
+          this.getInstructor();
+        })
+      }
+    });
   }
+
+
 
 }
