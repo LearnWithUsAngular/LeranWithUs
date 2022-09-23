affa0724fc5ac0e565b9bad30b63539702e45b5b
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-09-23 10:04:46 +0630

    database joining completed

--- backend/src/services/SearchService.ts
+++ backend/src/services/SearchService.ts
@@ -9,39 +9,49 @@ export const searchAllService = async (
   _next: NextFunction
 ) => {
   try {
-    // const search = await Course.aggregate([
-    //   {
-    //     '$lookup': {
-    //       //searching collection name
-    //       'from': 'instructors',
-    //       //setting variable [searchId] where your string converted to ObjectId
-    //       'let': { "searchId": { $toObjectId: "$instructor_id" } },
-    //       //search query with our [searchId] value
-    //       "pipeline": [
-    //         //searching [searchId] value equals your field [_id]
-    //         { "$match": { "$expr": [{ "_id": "$$searchId" }] } },
-    //         { "$match": { "$and": [
-    //           { "$or": [{ "instructorName": { '$regex': req.body.keyword, '$options': 'i' } }] },
-    //           { "$or": [{ "deleted_at": null }] }
-    //         ]} },
-
-    //         //projecting only fields you reaally need, otherwise you will store all - huge data loads
-    //         // { "$project": { "_id": 1, "instructorName": "instructorName" } }
-    //       ],
-    //       'as': 'instructorInfo'
-    //     }
-    //   },
-    //   // {
-    //   //   "$unwind": "$instructorInfo"  // $unwind used for getting data in object or for one record only
-    //   // },
-    // ]);
-    console.log(req.body.keyword)
-    const search = await Course.find({
-      $and: [
-        { $or: [{ title: { '$regex': req.body.keyword, '$options': 'i' } }] },
-        { $or: [{ deleted_at: null }] }
-      ]
-    });
+    const search = await Course.aggregate([
+      {
+        '$lookup': {
+          //searching collection name
+          'from': 'instructors',
+          // setting variable [searchId] where your string converted to ObjectId
+          'let': { "searchId": { $toObjectId: "$instructor_id" } },
+          //search query with our [searchId] value
+          "pipeline": [
+            //searching [searchId] value equals your field [_id]
+            { "$match": { "$expr": { "$eq": [ "$_id", "$$searchId" ] } } },
+            { "$match": { "$and": [
+              { "$or": [{ "instructorName": { '$regex': req.body.keyword, '$options': 'i' }}]},
+              { "$or": [{ "deleted_at": null }] }
+            ]} },
+            //projecting only fields you really need, otherwise you will store all - huge data loads
+            // { "$project": { "_id": 1, "instructorName": "instructorName" } }
+          ],
+          'as': 'instructor_id'
+        }
+      },
+      // {
+      //   "$unwind": "$instructor_id"  // $unwind used for getting data in object or for one record only
+      // },
+      {
+        '$lookup': {
+          'from': 'categories',
+          'let': { "searchId": "$detail.category_id" },
+          "pipeline": [
+            { "$match": { "$expr": { "$eq": [ "$_id", "$$searchId" ] } } },
+          ],
+          'as': 'detail.category_id'
+        }
+      }
+      
+    ]);
+    // const search = await Course.find({
+    //   $and: [
+    //     // { $or: [{ "detail.title": { '$regex': req.body.keyword, '$options': 'i' }}, { "category": { '$regex': req.body.keyword, '$options': 'i' }} ] },
+    //     { $or: [{ "detail.category": { '$regex': req.body.keyword, '$options': 'i' }} ] },
+    //     { $or: [{ deleted_at: null }] }
+    //   ]
+    // });
     res.json({ data: search, status: 1 })
   } catch (err) {
     console.log(err)
