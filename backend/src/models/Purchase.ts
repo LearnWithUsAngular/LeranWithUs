acf41734f2544e69942f1c77e0f42a04720e65db
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-10-11 15:22:26 +0630

    purchase api

---
+++ backend/src/models/Purchase.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const purchaseSchema = new Schema({
+  course_id: {
+    type: Schema.Types.ObjectId,
+    ref: "course",
+    autopopulate: true
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    autopopulate: true
+  },
+  deleted_at: {
+    type: Date
+  },
+},
+  {
+    timestamps: true
+  }
+);
+purchaseSchema.plugin(require('mongoose-autopopulate'));
+export default model("purchase", purchaseSchema)
